0bd7f7bccfb5b93a6a3e66ecd235131173cdf191
thanhluancn1 <thanhluancn1@gmail.com>
2025-11-22 11:54:35 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,38 @@
+// src/App.jsx
 import { Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import ClassManagement from "./pages/ClassManagement";
-import Student from "./pages/Student";
-import Schedule from "./pages/Schedule";
-import CreateExam from "./pages/CreateExam";
+
+// --- IMPORT MỚI (Đã đổi tên) ---
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+import ClassPage from "./pages/ClassPage";
+import StudentPage from "./pages/StudentPage";
+import SchedulePage from "./pages/SchedulePage";
+import ExamMatrixPage from "./pages/ExamMatrixPage";
 import ExamSuggestionPage from "./pages/ExamSuggestionPage";
+import AssignmentListPage from "./pages/AssignmentListPage";
 import AssignmentDetailPage from "./pages/AssignmentDetailPage";
-import AssignmentManagementPage from "./pages/AssignmentManagementPage";
-
-
 
 export default function App() {
   return (
     <MainLayout>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/classes" element={<ClassManagement />} />
-        <Route path="/students" element={<Student />} />
-        <Route path="/schedules" element={<Schedule />} />
-        <Route path="/exam" element={<CreateExam />} />
+        {/* Trang chung */}
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+
+        {/* Quản lý danh mục */}
+        <Route path="/classes" element={<ClassPage />} />
+        <Route path="/students" element={<StudentPage />} />
+        <Route path="/schedules" element={<SchedulePage />} />
+
+        {/* Module Đề thi & Bài tập */}
+        <Route path="/exam" element={<ExamMatrixPage />} />
         <Route path="/exam-suggestions" element={<ExamSuggestionPage />} />
-        <Route 
-          path="/assignment-detail/:batchId" 
-          element={<AssignmentDetailPage />} 
-        />
-        <Route 
-          path="/assignment-management" 
-          element={<AssignmentManagementPage />} 
-        />
+        <Route path="/assignment-management" element={<AssignmentListPage />} />
+        <Route path="/assignment-detail/:batchId" element={<AssignmentDetailPage />} />
       </Routes>
     </MainLayout>
   );
